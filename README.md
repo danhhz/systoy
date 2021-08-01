87741ac4409bbede6283c660ffc7c89793b1f2d3
Daniel Harrison <52528+danhhz@users.noreply.github.com>
2021-08-01 11:13:39 -0700

    rs: add variant that syncs once after all writes

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # systoy
 
 - Rust: Run on MacBook Air (M1, 2020) 16GB
-  - <a href="https://danhhz.github.io/systoy/rs/criterion/serial/report/index.html">serial</a>: file write followed by immediate sync
+  - <a href="https://danhhz.github.io/systoy/rs/criterion/file_log_append/report/index.html">file_log_append</a>
+    - serial: file write followed by immediate datasync
+    - sync_at_end: one datasync after all file writes
--- rs/benches/file.rs
+++ rs/benches/file.rs
@@ -6,10 +6,17 @@ use std::iter;
 use std::time::Instant;
 
 use criterion::measurement::WallTime;
-use criterion::{criterion_group, criterion_main, Bencher, BenchmarkId, Criterion, Throughput};
+use criterion::{
+    criterion_group, criterion_main, AxisScale, Bencher, BenchmarkId, Criterion, PlotConfiguration,
+    Throughput,
+};
 
-fn buf_sizes() -> Vec<usize> {
-    iter::successors(Some(1usize), |x| Some(*x * 2)).take_while(|x| *x <= 32 * 1024).collect()
+fn interesting_buf_sizes() -> Vec<usize> {
+    vec![
+        1,           // Worst case
+        4 * 1024,    // Disk page size,
+        1024 * 1024, // Big-ish write
+    ]
 }
 
 fn buf_data(size_bytes: usize) -> Vec<u8> {
@@ -32,14 +39,33 @@ fn serial_run(b: &mut Bencher<WallTime>, buf_size: &usize) {
     });
 }
 
-fn serial(c: &mut Criterion) {
-    let mut group = c.benchmark_group("serial");
-    for buf_size in buf_sizes().into_iter() {
+fn sync_at_end_run(b: &mut Bencher<WallTime>, buf_size: &usize) {
+    let data = buf_data(*buf_size);
+    let mut f = tempfile::tempfile().unwrap();
+    b.iter_custom(|iters| {
+        let start = Instant::now();
+        for _i in 0..iters {
+            f.write_all(&data).unwrap();
+        }
+        f.sync_data().unwrap();
+        start.elapsed()
+    });
+}
+
+fn file_log_append(c: &mut Criterion) {
+    let mut group = c.benchmark_group("file_log_append");
+    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+    for buf_size in interesting_buf_sizes().into_iter() {
         group.throughput(Throughput::Bytes(buf_size as u64));
-        group.bench_with_input(BenchmarkId::from_parameter(buf_size), &buf_size, serial_run);
+        group.bench_with_input(BenchmarkId::new("serial", buf_size), &buf_size, serial_run);
+        group.bench_with_input(
+            BenchmarkId::new("sync_at_end", buf_size),
+            &buf_size,
+            sync_at_end_run,
+        );
     }
     group.finish();
 }
 
-criterion_group!(benches, serial);
+criterion_group!(benches, file_log_append);
 criterion_main!(benches);
